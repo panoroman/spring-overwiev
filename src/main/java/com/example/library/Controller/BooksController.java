969a5be0d62cc8373b6d41b6afa4a7e188fa0482
Roman Panfilov <roman.panfilov@softservice.org>
2022-01-26 23:04:00 +0700

    add put, patch, delete methods

--- src/main/java/com/example/library/Controller/BooksController.java
+++ src/main/java/com/example/library/Controller/BooksController.java
@@ -2,33 +2,60 @@ package com.example.library.Controller;
 
 import com.example.library.Model.Books;
 import com.example.library.Repository.BooksRepository;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/books/")
+@RequestMapping("/")
 public class BooksController {
 
     @Autowired
     private BooksRepository booksRepository;
 
-    @GetMapping("/{id}")
+    @GetMapping("/books")
     public List<Books> getAllBooks() {
-        return this.booksRepository.findAll();
+        return booksRepository.findAll();
     }
 
-//    @GetMapping("/books/{id}")
-//    public Resp
-
-    @PostMapping("/")
-    public Books createBooks(@RequestBody Books books) {
-        return this.booksRepository.save(books);
+    @GetMapping("/books/{id}")
+    public Optional<Books> getBook(@PathVariable long id) {
+        return booksRepository.findById(id);
     }
 
-//    @PutMapping("books/{id}")
-//    public ResponseEntity<Books> updateBooks(@RequestBody Books books) {
-//        return this.booksRepository.save(books);
-//    }
+    @PostMapping("/books")
+    public Books createBook(@RequestBody Books books) {
+        return booksRepository.save(books);
+    }
+
+    @PutMapping("/books/{id}")
+    public Books updateBook(@RequestBody Books updBooks, @PathVariable long id) {
+        return booksRepository.findById(id).map(book -> {
+            book.setAuthor(updBooks.getAuthor());
+            book.setTitle(updBooks.getTitle());
+            return booksRepository.save(book);
+        }).orElseGet(() -> booksRepository.save(updBooks));
+    }
+
+    @SneakyThrows
+    @PatchMapping("/books/{id}")
+    public Books patchBook(@RequestBody Books updBooks, @PathVariable long id) {
+        Books book = booksRepository.findById(id).orElseThrow(ChangeSetPersister.NotFoundException::new);
+        if(updBooks.getAuthor() != null) {
+            book.setAuthor(updBooks.getAuthor());
+        }
+        if(updBooks.getTitle() != null) {
+            book.setTitle(updBooks.getTitle());
+        }
+        return booksRepository.save(book);
+    }
+
+    @DeleteMapping("/books/{id}")
+    public void deleteBook(@PathVariable long id) {
+        booksRepository.deleteById(id);
+    }
 }
